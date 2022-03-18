a965fa536d31d4a191cea3291570d2e186da0c00
Wael Elkadeem <elkadeem@hotmail.com>
2022-03-18 07:26:33 +0000

    Add MVC with Identity

---
+++ MvcAppIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcAppIdentity.Startup))]
+namespace MvcAppIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
